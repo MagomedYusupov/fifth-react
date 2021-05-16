6d804e6d341ad9ba64e1e58cd99ab23afb1d53da
yusupov <yusupov.islam@mail.ru>
2021-05-16 19:31:12 +0300

    first-commit

---
+++ src/form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Form(props) {
+    return (
+        <div className="form">
+            <input placeholder="Введите текст..."
+                   type="text"
+                   value={props.text}
+                   onChange={(e)=>props.setText(e.target.value)}
+            />
+            <button onClick={props.addTodo}>
+                добавить
+            </button>
+        </div>
+    );
+}
+
+export default Form;
